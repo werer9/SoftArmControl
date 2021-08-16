f727f0db6ca2d34b3f467cb878613455b85c551e
Caelan Murch <werer9@gmail.com>
2021-08-16 18:06:50 +1200

    merged main functions

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from Chatbot.chatbot import *
+from yolov5.depth import *
 
 if __name__ == '__main__':
-    chatbot = Chatbot("robotarm-315611", "123", "en")
-    print(chatbot.get_user_intent([input("Hello, what do you want to do?\n")]))
-
+    depth()
